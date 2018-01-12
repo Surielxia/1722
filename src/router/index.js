b8905165d8ea7d32b24bb0d1d19ab084c0d2f001
xia <530084905@qq.com>
2018-01-12 15:17:37 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index'
 import City from '@/pages/city'
+import Order from '@/pages/order/order'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/city',
       name: 'City',
       component: City
+    }, {
+      path: '/order',
+      name: 'Order',
+      component: Order
     }
   ]
 })
